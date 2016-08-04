21820532220cd151820f73b35ebdb2a28da3f1f3
anoopemacs <anoopemacs@gmail.com>
2016-08-04 15:11:40 +0530

    4-add a form to add tasks & an event to handle submission

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -6,6 +6,23 @@ import './body.html';
 
 Template.body.helpers({
   tasks() {
-    return Tasks.find({});
+    return Tasks.find({}, { sort: { createdAt: -1 } });
   },
+});
+
+Template.body.events({
+  'submit .new-task'(event) {
+    event.preventDefault();
+
+    const target = event.target; // target here is the <form>
+    const text = target.text.value;
+
+    Tasks.insert({
+      text,			// why is there no key name here
+      createdAt: new Date(),
+    });
+
+    target.text.value = '';
+    target.text.focus();
+  }
 });
